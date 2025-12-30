ee46ba5a58543a8bbdd2b930a1d5b0f155522bb1
akhanrex <akhanrex@gmail.com>
2025-12-30 11:48:06 +0530

    your message

--- ops/refresh_token.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")/.."
-source .venv/bin/activate
-python -m probedge.ops.get_kite_token
--- ops/run_live.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")/.."
-source .venv/bin/activate
-export MODE=live
-uvicorn apps.api.main:app --host 127.0.0.1 --port 8000 --reload
